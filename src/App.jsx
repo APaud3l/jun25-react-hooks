35104cc0fd39280821e81a4383380b097d638e05
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-12-13 10:07:31 +1100

    feat: Initialise with a very simple React app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{ padding: "2rem" }}>
+        <h1>React Hooks Lab Dashboard</h1>
+        <p>Playground for useState, useEffect, and others.</p>
+
+        <section>
+          <h2>1. State and Effect Panel</h2>
+          {/* We'll drop some component here. */}
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App;
